cf99b80e8188373739efc9d5e142fc261cfd91cd
Dheeraj Joshi <djsays124@gmail.com>
2026-07-14 15:03:36 +0530

    add access control for mint and burn permissions

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -5,18 +5,25 @@
 pragma solidity ^0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract RebaseToken is ERC20 {
+contract RebaseToken is ERC20, Ownable, AccessControl {
     error RebaseToken_InterestRateCanOnlyDecrease(uint256 oldInterestRate, uint256 newInterestRate);
     uint256 private s_interestRate = 5e10;
     uint256 constant PRECISION_FACTOR = 1e18;
+    bytes32 private constant MINT_AND_BURN_ROLE = keccak256("MINT_AND_BURN_ROLE");
     mapping (address => uint256) private s_userInterestRate;
     mapping (address => uint256) private s_userLastUpdatedTimeStamp;
 
     event InterestRateSet(uint256 newInterestRate);
-    constructor() ERC20 ("Rebase Token" , "RBT") {}
+    constructor() ERC20 ("Rebase Token" , "RBT") Ownable(msg.sender) {}
 
-    function setInterestRate(uint256 _newInterestRate) external {
+    function grantMintAndBurnRole(address _account) external onlyOwner {
+        _grantRole(MINT_AND_BURN_ROLE, _account);
+    }
+
+    function setInterestRate(uint256 _newInterestRate) external onlyOwner {
         if(_newInterestRate < s_interestRate) {
             revert RebaseToken_InterestRateCanOnlyDecrease(s_interestRate, _newInterestRate);
         }
@@ -30,13 +37,13 @@ contract RebaseToken is ERC20 {
 
 
 
-    function mint(address _to, uint256 _amount) external {
+    function mint(address _to, uint256 _amount) external onlyRole(MINT_AND_BURN_ROLE) {
         _mintAccurateInterest(_to);
         s_userInterestRate[_to] = s_interestRate;
         _mint(_to, _amount);
     }
 
-    function burn (address _from , uint256 _amount) external {
+    function burn (address _from , uint256 _amount) external onlyRole(MINT_AND_BURN_ROLE) {
         if(_amount > type(uint256).max) {
             _amount = balanceOf(_from);
         }
